3001f5e224696a131a2d895f47abca59c069fe8c
omer-os <omerchetin19@gmail.com>
2026-02-22 03:27:10 +0300

    .

---
+++ +page.svelte
@@ -0,0 +1,9 @@
+" ============================================================================
+" Netrw Directory Listing                                        (netrw v184)
+"   /home/omar/Desktop/projects/svelte-tut/src/routes/auth/login
+"   Sorted by      name
+"   Sort sequence: [\/]$,*,\(\.bak\|\~\|\.o\|\.h\|\.info\|\.swp\|\.obj\)[*@]\=$
+"   Quick Help: <F1>:help  -:go up dir  D:delete  R:rename  s:sort-by  x:special
+" ==============================================================================
+../
+./
